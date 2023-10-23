30c4cb0883d6fbf0ce148caf578c045e6c32787b
Torremolinos <adrian_2054@hotmail.com>
2023-10-24 01:00:09 +0200

    empezando el js

---
+++ scripts/pizzas.js
@@ -0,0 +1,10 @@
+export { pizzas };
+
+let cantidad = () =>{
+    botonCantidad.document.getElementById("")
+    const input = parseInt(document.getElementById("input").value);
+};
+
+const pizzas = [
+{id: 1 , nombre : "Ricota Romana", precio: 10, ingredientes: `Tomate, mozzarella buffala, Jamón serrano, oregano.`,cantidad: cantidad()  }
+];
